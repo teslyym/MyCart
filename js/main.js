d64f314835240889b919759a87fc376469c04b6f
Teslim <steslimolayiwola@gmail.com>
2024-02-21 14:20:08 +0100

    addition of functionalities

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const items = document.querySelectorAll('.cart1')
 
+function calculatetotal() {
+    let subtotal =document.querySelector('.Subtotal')
+    let tax =document.querySelector('.tax')
+    let grandtotal =document.querySelector('.gtotal')
+    let sub=0
+    items.forEach(item => {
+        const total =item.querySelector('.total')
+        sub+=parseFloat(total.innerHTML)
+    }) 
+    subtotal.innerHTML = `$${sub}`
+    grandtotal.innerHTML = `$${sub+ parseFloat(tax.innerHTML)}`
+}
+
 items.forEach(item => {
     const dec =item.querySelector('.decrement')
+    const total =item.querySelector('.total')
     const inc =item.querySelector('.increment')
     const qty =item.querySelector('.quantity')
     const price =item.querySelector('.price')
-    const total =item.querySelector('.total')
     let quantity = parseInt(qty.innerHTML)
 
     inc.addEventListener('click', ()=>{
@@ -13,6 +26,7 @@ items.forEach(item => {
         quantity++
         qty.innerHTML =quantity
         total.innerHTML = quantity *price.innerHTML
+        calculatetotal()
     })
     
     dec.addEventListener('click', ()=>{
@@ -25,5 +39,8 @@ items.forEach(item => {
         qty.innerHTML =quantity
         total.innerHTML = quantity *price.innerHTML
     })
-    
+    calculatetotal()
 });
+
+
+calculatetotal()
